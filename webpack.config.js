71fb100defa84476703310b21e2f515378c0724f
williamsjs <williamsjs1224@gmail.com>
2018-04-10 19:43:11 -0400

    add react to webpack config and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['env', 'react']
           }
         }
       }
