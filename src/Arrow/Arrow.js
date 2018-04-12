433c04a561a510ad5eddc1602c0207b2fff4c186
williamsjs <williamsjs1224@gmail.com>
2018-04-11 20:28:35 -0400

    add arrow component and react-fa

---
+++ src/Arrow/Arrow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Arrow.css';
+
+const Arrow = () => {
+  return (
+    <div className="arrow"></div>
+  );
+}
+
+export default Arrow;
